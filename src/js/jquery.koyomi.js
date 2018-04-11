5dd2a2151635933e6481b2f5edb18d7ab3e70a11
today4u <today4u@users.noreply.github.com>
2018-04-11 22:52:30 +0900

    add lang: short name

--- src/js/jquery.koyomi.js
+++ src/js/jquery.koyomi.js
@@ -251,23 +251,28 @@
         switch(lang) {
             case 'en':
                 return {
-                    dow:   ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-                    month: ['January','February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+                    dow:        ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+                    dowShort:   ['Sun',    'Mon',    'Tue',     'Wed',       'Thu',      'Fri',    'Sat'],
+                    month:      ['January','February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                    monthShort: ['Jan',    'Feb',      'Mar',   'Apr',   'May', 'Jun',  'Jul',  'Aug',    'Sep',       'Oct',     'Nov',      'Dec']
                 };
             case 'ja':
                 return {
-                    dow:   ['日', '月', '火', '水', '木', '金', '土'],
-                    month: ['1月','2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
+                    dow:        ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'],
+                    dowShort:   ['日',     '月',     '火',     '水',     '木',     '金',     '土'],
+                    month:      ['1月','2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
                 };
             case 'ko':
                 return {
-                    dow:   ['일', '월', '화', '수', '목', '금', '토'],
-                    month: ['일월','이월', '삼월', '사월', '오월', '유월', '칠월', '팔월', '구월', '시월', '십일월', '십이월']
+                    dow:        ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
+                    dowShort:   ['일',     '월',     '화',     '수',     '목',     '금',     '토'],
+                    month:      ['일월','이월', '삼월', '사월', '오월', '유월', '칠월', '팔월', '구월', '시월', '십일월', '십이월']
                 };
             case 'zh':
                 return {
-                    dow:   ['日', '一', '二', '三', '四', '五', '六'],
-                    month: ['一月','二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月']
+                    dow:        ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
+                    dowShort:   ['日',     '一',     '二',     '三',     '四',     '五',     '六'],
+                    month:      ['一月','二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月']
                 };
         }
     }
